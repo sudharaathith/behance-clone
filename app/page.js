ab96f3afcc49b3eb7688eab8f58bb5af2dece71a
sudharaathith <t.sudharaathith@gmail.com>
2023-11-18 16:19:56 +0530

    banner complited

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import NavBar from "@/components/NavBar";
 import bgimg from "../image/search-banner-image.jpeg";
 import BannerItem from "@/components/BannerItem";
 import Share from "@/SVG/Share";
+import Sell from "@/SVG/Sell";
+import Job from "@/SVG/Job";
 
 export default function Home() {
   return (
@@ -21,8 +23,8 @@ export default function Home() {
           <h1 className="z-[2] text-center text-lg text-[22px] mb-9 text-white ">Join Behance, the world's largest creative network</h1>
           <div className="z-[2] flex justify-center gap-7 w-fit mx-auto lg:w-4/5 max-lg:mx-4">
             <BannerItem icon={Share}>Share your work and get discovered by millions of creatives and clients</BannerItem>
-            <BannerItem icon={Share}>Sell design assets and make money creating what you love</BannerItem>
-            <BannerItem icon={Share}>Find your next full time or freelance opportunity</BannerItem>
+            <BannerItem icon={Sell}>Sell design assets and make money creating what you love</BannerItem>
+            <BannerItem icon={Job}>Find your next full time or freelance opportunity</BannerItem>
 
           </div>
         </div>
