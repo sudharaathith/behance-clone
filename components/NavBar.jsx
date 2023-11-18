b15cdb8466bceae5dc60b1cbdbd2c87264182b58
sudharaathith <t.sudharaathith@gmail.com>
2023-11-18 11:43:12 +0530

    init

---
+++ components/NavBar.jsx
@@ -0,0 +1,30 @@
+import Bell from "@/SVG/Bell"
+
+
+function NavBar() {
+  return (
+    <div className="flex justify-between px-3 items-center border">
+        <div className=" flex items-baseline gap-3 font-semibold text-md">
+            <div className="text-xl font-bold">Behance</div>
+            <NavItem>For You</NavItem>
+            <NavItem>Discover</NavItem>
+            <NavItem>Hire</NavItem>
+            <NavItem>Assets</NavItem>
+            <NavItem>Jobs</NavItem>
+            </div>
+        <div className="flex gap-1 mx-3 items-center">
+            <div className="mx-3"><Bell className="w-5 h-5 hover:fill-slate-500 cursor-pointer"/></div>
+            <div><button className="px-4 border border-[#dee8ff] text-[#0057ff] text-[14px] font-semibold py-1 hover:border-[#b7cdff] hover:bg-[#dee8ff] rounded-full  bg-[#f5f8ff]">Log In</button></div>
+            <div><button className="px-4 border border-transparent bg-[#0057ff] text-[14px] font-semibold py-1 hover:bg-[#003ecb] rounded-full  text-[#f5f8ff]">Sign Up</button></div>
+        </div>
+    </div>
+  )
+}
+
+function NavItem(props){
+    return (
+        <div className="py-4 hover:border-b-2 border-black hover:cursor-pointer">{props.children}</div>
+    )
+}
+
+export default NavBar
