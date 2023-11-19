597b98a9b11f8ecab98dc265a2ea3e1d9885e034
sudharaathith <t.sudharaathith@gmail.com>
2023-11-19 19:15:07 +0530

    search bar

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,71 +1,75 @@
-import Search from "@/SVG/Search"
+import { useState, useRef, useEffect, useCallback } from "react";
+import Search from "@/SVG/Search";
 import { words } from "@/utils/Autocommplet";
-import { useEffect, useRef, useState } from "react"
 
 function SearchBar() {
     const [text, setText] = useState("");
-    const [autocomplet, setAutocomplet] = useState([]);
+    const [autocomplete, setAutocomplete] = useState([]);
     const [open, setOpen] = useState(false);
-    const ref = useRef(0);
-    useEffect(() => {
-        var reg = new RegExp(text)
-        setAutocomplet(words.filter(function (term) {
-            if (term.match(reg)) {
-                return term;
-            }
-        }).slice(0, 5))
-    }, [text])
-    return (
-        <div className={` flex w-full py-2 gap-2 border-r items-center relative rounded-l-full border`}>
-            <div className="flex items-center pl-4 w-full">
+    const inputRef = useRef(null);
 
+    const debounce = (func, delay) => {
+        let timeoutId;
+        return function (...args) {
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => {
+                func.apply(this, args);
+            }, delay);
+        };
+    };
+
+    const debouncedSearch = useCallback(
+        debounce((searchText) => {
+            const reg = new RegExp(searchText, 'i');
+            setAutocomplete(words.filter(term => term.match(reg)).slice(0, 5));
+        }, 300), []
+    );
+
+    useEffect(() => {
+        if (text !== "") {
+            debouncedSearch(text);
+        } else {
+            setAutocomplete([]);
+        }
+    }, [text, debouncedSearch]);
+
+    const handleFocus = () => {
+        setOpen(true);
+    };
+
+    const handleBlur = () => {
+        setOpen(false);
+    };
+
+    const handleInputChange = (e) => {
+        setText(e.target.value);
+    };
+
+    return (
+        <div className="flex w-full py-2 gap-2 border-r items-center relative rounded-l-full border"
+        style={{borderRadius: open?"12px 0px 0px":""}}>
+            <div className="flex items-center pl-4 w-full">
                 <Search className="w-5 h-5 fill-gray-500" />
                 <input
+                    ref={inputRef}
                     className="w-full self-end font-semibold text-lg outline-none"
                     type="text"
                     placeholder="Search the creative world at work"
                     value={text}
-                    onChange={(e) => {
-                        setText(e.target.value);
-                    }}
-                    onFocus={(e) => {
-                        setOpen(true);
-                        const c = document.querySelector("summa");
-                        console.log(c)
-                    }}
-
+                    onChange={handleInputChange}
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
                 />
             </div>
-            {
-                open && <div  className="flex flex-col pl-4 top-0 absolute w-full border rounded-tr-none pt-2 bg-white rounded-2xl">
-                    <div className="flex items-center">
-
-                        <Search className="w-5 h-5 fill-gray-500" />
-                        <input
-                            id="summa"
-                            className="w-full self-end font-semibold text-lg summa outline-none"
-                            type="text"
-                            placeholder="Search the creative world at work"
-                            value={text}
-                            onChange={(e) => {
-                                setText(e.target.value);
-                            }}
-                            onFocus={(e) => {
-                                setOpen(true);
-                            }}
-                            onBlur={() => {
-                                setOpen(false);
-                            }}
-                        />
-                    </div>
-                    <div>
-                        hi
-                    </div>
+            {open && (
+                <div className="flex flex-col pl-4 top-8 absolute w-full border border-t-0 rounded-t-none pt-2 bg-white rounded-2xl">
+                    {autocomplete.map((result, index) => (
+                        <div key={index}>{result}</div>
+                    ))}
                 </div>
-            }
-
+            )}
         </div>
-    )
+    );
 }
 
-export default SearchBar
+export default SearchBar;
