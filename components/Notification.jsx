6f52f277b203e5153d6794f7936356500435085c
sudharaathith <t.sudharaathith@gmail.com>
2023-11-19 09:12:53 +0530

    summa

--- components/Notification.jsx
+++ components/Notification.jsx
@@ -8,10 +8,10 @@ export function Notification(props) {
     <HoverCard.Root openDelay={75} closeDelay={150} onOpenChange={(open)=>{
         document.querySelector("main").style = (open)?"filter: brightness(80%);":"filter: brightness(100%);"
     }}>
-      <HoverCard.Trigger >
+      <HoverCard.Trigger>
         <Bell className="w-5 h-5 hover:fill-slate-500 cursor-pointer" />
       </HoverCard.Trigger>
-      <HoverCard.Portal>
+      <HoverCard.Portal >
         <HoverCard.Content
           className=" data-[side=bottom]:animate-slideUpAndFade data-[side=right]:animate-slideLeftAndFade data-[side=left]:animate-slideRightAndFade data-[side=top]:animate-slideDownAndFade w-[400px] rounded-md bg-white shadow-2xl data-[state=open]:transition-all"
           sideOffset={5}
@@ -24,7 +24,6 @@ export function Notification(props) {
             <div className="p-5 h-40 text-center text-sm">You have no new notifications.</div>
           </div>
 
-          <HoverCard.Arrow className="fill-white" />
         </HoverCard.Content>
       </HoverCard.Portal>
     </HoverCard.Root>
--- components/SearchArea.jsx
+++ components/SearchArea.jsx
@@ -1,46 +1,147 @@
-"use client"
+"use client";
 import Edit from "@/SVG/Edit";
 import Search from "@/SVG/Search";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import Options from "./Options";
+
+const op = [
+  {name:"Recommended"},
+  {name:"Curated"},
+  {name:"Most Appreciated"},
+  {name:"Most Viewed"},
+  {name:"Most Discussed"},
+]
 
 function SearchArea() {
-    const ref = useRef(0);
-    const topOffset = useRef(false)
-    useEffect(() => {
-        const handleScroll = () => {
-          if (ref.current) {
-            if (window.innerWidth >= 768) { // Assuming 'md' screen size in Tailwind is 768px
-              if (!topOffset.current) {
-                topOffset.current = ref.current.getBoundingClientRect().top + window.pageYOffset - 46; // Set the desired top offset in pixels
-              }
-              
-              const scrollY = window.scrollY || window.pageYOffset;
-              if (scrollY > topOffset.current) {
-                ref.current.style.position = "fixed";
-                ref.current.style.top = `57px`;
-              } else {
-                ref.current.style.position = "static";
-              }
-            }
+  const ref = useRef(0);
+  const topOffset = useRef(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (ref.current) {
+        if (window.innerWidth >= 768) {
+          // Assuming 'md' screen size in Tailwind is 768px
+          if (!topOffset.current) {
+            topOffset.current =
+              ref.current.getBoundingClientRect().top + window.pageYOffset - 46; // Set the desired top offset in pixels
           }
-        };
-    
-        window.addEventListener("scroll", handleScroll);
-        return () => {
-          window.removeEventListener("scroll", handleScroll);
-        };
-      }, []);
-    
+
+          const scrollY = window.scrollY || window.pageYOffset;
+          if (scrollY > topOffset.current) {
+            ref.current.style.position = "fixed";
+            ref.current.style.top = `57px`;
+          } else {
+            ref.current.style.position = "static";
+          }
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div ref={ref} className="flex py-5 shadow-md  bg-white w-full max-md:fixed max-md:top-[57px]">
-      <div>
-        <Edit />
-        <button>Filter</button>
+    <div
+      ref={ref}
+      className="flex flex-col py-5 shadow-md justify-center items-center  bg-white w-full max-md:fixed max-md:top-[57px] px-3"
+    >
+      <div className="flex w-full rounded-full border items-center px-3 gap-2">
+        <div className="flex w-full items-center py-1 gap-2 border-r">
+          <Search className="w-5 h-5 fill-gray-500" />
+          <input
+            className="w-full self-end font-semibold text-lg outline-none"
+            type="text"
+            placeholder="Search the creative world at work"
+          />
+        </div>
+        <Filter />
       </div>
-      <div className="flex w-full rounded-full border items-center py-1 px-3 gap-2">
-        <Search className="w-5 h-5 fill-gray-500" />
-        <input className="w-full self-end font-semibold text-lg outline-none" type="text" placeholder="Search the creative world at work" />
+      <div className="flex w-full">
+        <div className="ml-auto">
+        <Options name="sort" options={op}/>
+        </div>
       </div>
+    </div>
+  );
+}
+
+function FilterItem(props) {
+  return (
+    <div
+      onClick={props.onClick}
+      className={` font-semibold text-[10px] cursor-pointer ${
+        props.select
+          ? "bg-black text-white"
+          : "bg-white text-black hover:bg-gray-100"
+      } px-3 rounded-full py-1`}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+function Filter(props) {
+  const [select, setSelect] = useState(0);
+  return (
+    <div className=" flex gap-1 px-1">
+      <FilterItem
+        select={select == 0}
+        onClick={() => {
+          setSelect(0);
+        }}
+      >
+        Projects
+      </FilterItem>
+      <FilterItem
+        select={select == 1}
+        onClick={() => {
+          setSelect(1);
+        }}
+      >
+        Assets
+      </FilterItem>
+      <FilterItem
+        select={select == 2}
+        onClick={() => {
+          setSelect(2);
+        }}
+      >
+        Images
+      </FilterItem>
+      <FilterItem
+        select={select == 3}
+        onClick={() => {
+          setSelect(3);
+        }}
+      >
+        Prototype
+      </FilterItem>
+      <FilterItem
+        select={select == 4}
+        onClick={() => {
+          setSelect(4);
+        }}
+      >
+        Streams
+      </FilterItem>
+      <FilterItem
+        select={select == 5}
+        onClick={() => {
+          setSelect(5);
+        }}
+      >
+        People
+      </FilterItem>
+      <FilterItem
+        select={select == 6}
+        onClick={() => {
+          setSelect(6);
+        }}
+      >
+        Moodbord
+      </FilterItem>
     </div>
   );
 }
